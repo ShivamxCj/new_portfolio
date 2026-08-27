0685bd2965f3b099364b13876fd7b7776a8d43ec
ShivamxCj <shivam.chatterjee.30jun@gmail.com>
2026-08-27 21:37:13 +0530

    added new certificate

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
-import certImage from "../../src/assets/certificate.jpg"; // 🔥 Add your certificate image here
+import certImage1 from "../../src/assets/certificate1.png"; // certificate1 image here
+import certImage2 from "../../src/assets/certificate2.jpg"; // certificate2 image here
 import { FiExternalLink } from "react-icons/fi";
 
 const Certificates = () => {
@@ -12,12 +13,14 @@ const Certificates = () => {
       <div className="max-w-6xl mx-auto">
         {/* Heading */}
         <h2 className="text-3xl md:text-5xl font-bold text-center text-teal-300 mb-16">
-          My Certification
+          My Certifications
         </h2>
 
-        {/* Certificate Layout */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
-          
+                                         {/* Certificate Layout */}
+
+        {/*-----------------------------------CERTIFICATE 1------------------------------------------*/}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center ">
+
           {/* Certificate Image (Left on Desktop, Bottom on Mobile) */}
           <motion.div
             initial={{ x: -80, opacity: 0 }}
@@ -27,7 +30,58 @@ const Certificates = () => {
             className="order-2 md:order-1"
           >
             <img
-              src={certImage}
+              src={certImage1}
+              alt="Certificate"
+              className="rounded-xl shadow-[0_0_30px_rgba(20,184,166,0.5)] border border-teal-400/30 hover:scale-105 transition duration-300 mb-6"
+            />
+          </motion.div>
+
+          {/* Certificate Details (Right on Desktop, Top on Mobile) */}
+          <motion.div
+            initial={{ x: 80, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            viewport={{ once: true }}
+            className="order-1 md:order-2"
+          >
+            <h3 className="text-2xl md:text-3xl font-semibold text-teal-300 mb-4">
+              SQL (Advanced)
+            </h3>
+
+            <p className="text-gray-300 mb-4">
+              This certification validates my proficiency in Advanced Topics of SQL like window functions, complex joins, Time Series etc.
+            </p>
+
+            <p className="text-gray-400 mb-6">
+              Issued by: HackerRank <br />
+              Year: 2026
+            </p>
+
+            <a
+              href="https://www.hackerrank.com/certificates/iframe/aba8acc17fb8"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-2 px-6 py-3 bg-teal-600/80 rounded-lg hover:bg-teal-700 transition shadow-lg"
+            >
+              View Credential
+              <FiExternalLink />
+            </a>
+          </motion.div>
+        </div>
+
+        {/*------------------------------CERTIFICATE 2---------------------------------------*/}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+
+          {/* Certificate Image (Left on Desktop, Bottom on Mobile) */}
+          <motion.div
+            initial={{ x: -80, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            viewport={{ once: true }}
+            className="order-2 md:order-1"
+          >
+            <img
+              src={certImage2}
               alt="Certificate"
               className="rounded-xl shadow-[0_0_30px_rgba(20,184,166,0.5)] border border-teal-400/30 hover:scale-105 transition duration-300"
             />
@@ -46,7 +100,7 @@ const Certificates = () => {
             </h3>
 
             <p className="text-gray-300 mb-4">
-                This certification validates my proficiency in SQL and relational database management, demonstrating my ability to design, query, and optimize databases effectively.
+              This certification validates my proficiency in SQL and relational database management, demonstrating my ability to design, query, and optimize databases effectively.
             </p>
 
             <p className="text-gray-400 mb-6">
@@ -64,8 +118,8 @@ const Certificates = () => {
               <FiExternalLink />
             </a>
           </motion.div>
-
         </div>
+
       </div>
     </section>
   );
